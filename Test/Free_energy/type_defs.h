5b01702428d06fda69c15ddf8e2312f351a64e61
seanbsm <seanbsm@outlook.com>
2020-09-07 12:50:21 +0200

    Forgot to git-add files

---
+++ Test/Free_energy/type_defs.h
@@ -0,0 +1,14 @@
+
+#ifndef TYPE_DEFS_H
+#define TYPE_DEFS_H
+
+#include <complex>
+
+typedef unsigned int uint;
+//~ typedef float floatType;
+typedef double floatType;
+typedef std::complex<floatType> cfloatType;
+typedef std::complex<double> cdouble;
+
+#endif // TYPE_DEFS_H
+

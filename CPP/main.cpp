f06949421a38f9e065d41de36c132e429ae27185
seanbsm <seanbsm@outlook.com>
2021-09-04 13:37:50 +0200

    BACKUP - not getting correct results in another platform

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -472,72 +472,73 @@ int main(int argc, char* argv[]){
 				double Eq_upper = 0.5*(q_WP_array[q_WP_idx+1] * q_WP_array[q_WP_idx+1])/mu1(E_bound);
 				double E_com = 0.5*(Eq_upper + Eq_lower);
 				
-				//double q_l     = com_energy_to_com_q_momentum(Eq_lower);
-				//double q_u     = com_energy_to_com_q_momentum(Eq_upper);
-				//double q_m     = com_energy_to_com_q_momentum(E_com);
-				//double T_lab_l = com_momentum_to_lab_energy(  q_l, E_bound);
-				//double T_lab_u = com_momentum_to_lab_energy(  q_u, E_bound);
-				//double T_lab_m = com_momentum_to_lab_energy(  q_m, E_bound);
+				double q_l     = com_energy_to_com_q_momentum(Eq_lower);
+				double q_u     = com_energy_to_com_q_momentum(Eq_upper);
+				double q_m     = com_energy_to_com_q_momentum(E_com);
+				double T_lab_l = com_momentum_to_lab_energy(  q_l, E_bound);
+				double T_lab_u = com_momentum_to_lab_energy(  q_u, E_bound);
+				double T_lab_m = com_momentum_to_lab_energy(  q_m, E_bound);
+				std::vector<double> T_lab_input_list = {1,2,3,4,5,9,10,13,22.7,35,53};
 				//std::vector<double> T_lab_input_list = {  3,   4,   5,       6,
 				//										  9,  10,  11,      12,
 				//										 13,  16,  22.7,    28,
 				//										 30,  35,  42,      47.5,
 				//										 50,  53,  65,      93.5,
 				//										146, 155, 180, 220, 240};
-				//for (size_t i=0; i<T_lab_input_list.size(); i++){
-				//	double Tlab_input = T_lab_input_list[i];
-				//	if (T_lab_l <= Tlab_input && Tlab_input <= T_lab_m){
-				//		if (num_T_lab==0 && q_WP_idx==0){
-				//			q_WP_idx_vec.push_back(q_WP_idx); num_T_lab += 1;
-				//		}
-				//		else if (num_T_lab==0 && q_WP_idx!=0){
-				//			q_WP_idx_vec.push_back(q_WP_idx-1); num_T_lab += 1;
-				//			q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
-				//		}
-				//		else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx-1){
-				//			q_WP_idx_vec.push_back(q_WP_idx); num_T_lab += 1;
-				//		}
-				//		else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx){
-				//			continue;
-				//		}
-				//		else{
-				//			q_WP_idx_vec.push_back(q_WP_idx-1); num_T_lab += 1;
-				//			q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
-				//		}
-				//	}
-				//	else if (T_lab_m <= Tlab_input && Tlab_input <= T_lab_u){
-				//		if (num_T_lab==0){
-				//			q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
-				//			q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
-				//		}
-				//		else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx){
-				//			q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
-				//		}
-				//		else{
-				//			q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
-				//			q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
-				//		}
-				//	}
-				//}
+				for (size_t i=0; i<T_lab_input_list.size(); i++){
+					double Tlab_input = T_lab_input_list[i];
+					if (T_lab_l <= Tlab_input && Tlab_input <= T_lab_m){
+						if (num_T_lab==0 && q_WP_idx==0){
+							q_WP_idx_vec.push_back(q_WP_idx); num_T_lab += 1;
+						}
+						else if (num_T_lab==0 && q_WP_idx!=0){
+							q_WP_idx_vec.push_back(q_WP_idx-1); num_T_lab += 1;
+							q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
+						}
+						else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx-1){
+							q_WP_idx_vec.push_back(q_WP_idx); num_T_lab += 1;
+						}
+						else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx){
+							continue;
+						}
+						else{
+							q_WP_idx_vec.push_back(q_WP_idx-1); num_T_lab += 1;
+							q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
+						}
+					}
+					else if (T_lab_m <= Tlab_input && Tlab_input <= T_lab_u){
+						if (num_T_lab==0){
+							q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
+							q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
+						}
+						else if (q_WP_idx_vec[num_T_lab-1]==q_WP_idx){
+							q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
+						}
+						else{
+							q_WP_idx_vec.push_back(q_WP_idx);   num_T_lab += 1;
+							q_WP_idx_vec.push_back(q_WP_idx+1); num_T_lab += 1;
+						}
+					}
+				}
 
-				if (E_com<1 && q_WP_idx%5!=0){
-					continue;
-				}
-				else if (E_com<10 && q_WP_idx%2!=0){
-					continue;
-				}
-				else if (E_com>200){
-					continue;
-				}
-				double q_com = com_energy_to_com_q_momentum(E_com);
-				double T_lab = com_momentum_to_lab_energy(q_com, E_bound);
-				if (T_lab<Tlab_min || Tlab_max<T_lab){
-					continue;
-				}
-				else{
-					q_WP_idx_vec.push_back(q_WP_idx);
-					num_T_lab += 1;
-				}
+				//if (E_com<1 && q_WP_idx%5!=0){
+				//	continue;
+				//}
+				//else if (E_com<10 && q_WP_idx%2!=0){
+				//	continue;
+				//}
+				//else if (E_com>200){
+				//	continue;
+				//}
+				//double q_com = com_energy_to_com_q_momentum(E_com);
+				//double T_lab = com_momentum_to_lab_energy(q_com, E_bound);
+				//if (T_lab<Tlab_min || Tlab_max<T_lab){
+				//	continue;
+				//}
+				//else{
+				//	q_WP_idx_vec.push_back(q_WP_idx);
+				//	num_T_lab += 1;
+				//}
 			}
 
 			T_lab_array = new double [num_T_lab];
